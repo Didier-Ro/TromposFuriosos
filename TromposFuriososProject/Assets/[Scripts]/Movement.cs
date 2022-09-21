c1996ea9b1ad1abdb512db9d16382b3182d8ccef
Didier-Ro <ortizrojeroh@gmail.com>
2022-09-21 13:05:36 -0500

    Animaciones

--- TromposFuriososProject/Assets/[Scripts]/Movement.cs
+++ TromposFuriososProject/Assets/[Scripts]/Movement.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody _rigidBody = default;
     [SerializeField] private float _speed = 5;
+    [SerializeField] private Animator _animator = default;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
     void Update()
     { 
-       transform.Translate(Input.acceleration.x * _speed * Time.deltaTime, 0, -Input.acceleration.z * _speed * Time.deltaTime);
+       _animator.SetFloat("Horizontal", Input.acceleration.x);
+       _animator.SetFloat("Vertical", Input.acceleration.z);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = new Vector3(Input.acceleration.x * _speed, _rigidBody.velocity.y, -Input.acceleration.z * _speed);
     }
 }
