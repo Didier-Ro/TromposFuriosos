ab0730fe9f5e2e85bff5333ab39d6f7b57af5e09
Didier-Ro <ortizrojeroh@gmail.com>
2022-09-26 13:00:51 -0500

    Enemy

---
+++ TromposFuriososProject/Assets/[Scripts]/Enemy/Impulse.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour
+{
+    private Rigidbody _rigidBody = default;
+    [SerializeField] private float impulseForce = 10;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _rigidBody.AddForce(transform.position - collision.gameObject.transform.position * impulseForce, ForceMode.Impulse);
+        }
+    }
+}
