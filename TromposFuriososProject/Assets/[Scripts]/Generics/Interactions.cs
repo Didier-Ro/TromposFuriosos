516e9932a0fef1a38ec46b84d3d996eac1cf2988
Didier-Ro <ortizrojeroh@gmail.com>
2022-10-18 20:56:58 -0500

    ParticleEffects

--- TromposFuriososProject/Assets/[Scripts]/Generics/Interactions.cs
+++ TromposFuriososProject/Assets/[Scripts]/Generics/Interactions.cs
@@ -7,6 +7,8 @@ public class Interactions : MonoBehaviour
 {
     [SerializeField] private string _objectTag = default;
     [SerializeField] private NavMeshController _navMeshController = default;
+    [SerializeField] private GameObject _powerUpEffectParticle = default;
+    [SerializeField] private GameObject _thunderParticleEffect = default;
 
     private void Start()
     {
@@ -19,6 +21,8 @@ public class Interactions : MonoBehaviour
         {
             collision.gameObject.SetActive(false);
             gameObject.tag = "MakeDamage";
+            _thunderParticleEffect.SetActive(true);
+            _powerUpEffectParticle.SetActive(true);
             _navMeshController._isPoweUpActive = false;
             StartCoroutine(PowerUpEffect());
         }
@@ -28,5 +32,7 @@ public class Interactions : MonoBehaviour
     {
         yield return new WaitForSeconds(10f);
         gameObject.tag = _objectTag;
+        _powerUpEffectParticle.SetActive(false);
+        _thunderParticleEffect.SetActive(false);
     }
 }
--- TromposFuriososProject/Assets/[Scripts]/Generics/MakeDamage.cs
+++ TromposFuriososProject/Assets/[Scripts]/Generics/MakeDamage.cs
@@ -7,9 +7,14 @@ public class MakeDamage : MonoBehaviour
     [SerializeField] private int _powerDamage = 10;
     [SerializeField] private string _tagToCompare = default;
     [SerializeField] private UnityEvent OnCollision = default;
+    [SerializeField] private GameObject _impactParticleEffect = default;
 
     private void OnCollisionEnter(Collision collision)
     {
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 position = contact.point;
+        Instantiate(_impactParticleEffect, position, rotation);
         if (collision.gameObject.CompareTag(_tagToCompare) && gameObject.CompareTag("MakeDamage"))
         {
             Health health = collision.gameObject.GetComponent<Health>();
